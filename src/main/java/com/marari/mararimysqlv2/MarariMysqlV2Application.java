ca8b321e9a0bd7c35450eb56cd0d9c06894df0e7
Matheus Vilvert <matheusvilvert@gmail.com>
2017-11-12 14:11:41 -0200

    primeiro commit

---
+++ src/main/java/com/marari/mararimysqlv2/MarariMysqlV2Application.java
@@ -0,0 +1,12 @@
+package com.marari.mararimysqlv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarariMysqlV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarariMysqlV2Application.class, args);
+	}
+}
